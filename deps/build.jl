40326d2da1a5e3e098016758c2316e5e993df233
Benoît Legat <benoit.legat@gmail.com>
2026-04-15 12:37:41 +0200

    Fix setup-java

--- deps/build.jl
+++ deps/build.jl
@@ -19,3 +19,24 @@ Downloads.download(MAXICP_URL, JAR_PATH; verbose=true)
 if isfile(JAR_PATH)
     write_depsfile(JAR_PATH)
 end
+
+# Compile the SearchHelper Java class needed for the MOI wrapper
+const HELPER_JAR = joinpath(@__DIR__, "maxicp_helper.jar")
+const JAVA_SRC = joinpath(@__DIR__, "java", "SearchHelper.java")
+if isfile(JAR_PATH) && isfile(JAVA_SRC)
+    javac = Sys.which("javac")
+    jar_cmd = Sys.which("jar")
+    if javac !== nothing && jar_cmd !== nothing
+        tmpdir = mktempdir()
+        try
+            run(`$javac -cp $JAR_PATH -d $tmpdir $JAVA_SRC`)
+            run(`$jar_cmd cf $HELPER_JAR -C $tmpdir SearchHelper.class`)
+        catch e
+            @warn "Failed to compile SearchHelper.java" exception = e
+        finally
+            rm(tmpdir; recursive = true, force = true)
+        end
+    else
+        @warn "javac or jar not found; SearchHelper.jar not built. MOI wrapper will not work."
+    end
+end
